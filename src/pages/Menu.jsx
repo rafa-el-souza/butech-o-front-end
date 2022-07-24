d5af083489c456f485805ed51a7c1be258e84d21
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-24 18:41:56 -0300

    feat: add app page boilerplates

---
+++ src/pages/Menu.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+// import MenuBackgroundPhoto from '../components/MenuBackgroundPhoto';
+// import MenuHeader from '../components/MenuHeader';
+// import MenuLogo from '../components/MenuLogo';
+// import MenuOpenClosed from '../components/MenuOpenClosed';
+// import MenuSearchBar from '../components/MenuSearchBar';
+// import MenuTitle from '../components/MenuTitle';
+// import SearchByCategory from '../components/SearchByCategory';
+// import SearchByKeyword from '../components/SearchByKeyword';
+
+function Menu() {
+  return (
+    <div>
+      Menu
+      {/* <MenuHeader>
+        <MenuLogo
+          logoURL="https://upload.wikimedia.org/wikipedia/commons/a/ab/Logo_TV_2015.png"
+          className="w-16 bg-slate-100 absolute top-48 left-8"
+        />
+        <MenuOpenClosed
+          className="absolute text-gray-200 bg-green-600 right-0 top-36"
+        />
+        <MenuTitle
+          title="butech-o"
+          className="absolute text-gray-200 text-2xl left-32 top-56"
+        />
+        <MenuBackgroundPhoto
+          bgPhotoURL="https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg"
+          className="relative w-screen -z-10"
+        />
+        <MenuSearchBar>
+          <SearchByKeyword />
+          <SearchByCategory />
+        </MenuSearchBar>
+      </MenuHeader>
+      <MenuCategoryList>
+        <MenuCategory>
+          <MenuProductList>
+            <MenuProduct>
+              <MenuProductPhoto />
+              <MenuProductTitle />
+              <MenuProductDescription />
+              <MenuProductPrice />
+              <CartInput />
+            </MenuProduct>
+          </MenuProductList>
+        </MenuCategory>
+      </MenuCategoryList> */}
+
+    </div>
+  );
+}
+
+export default Menu;
