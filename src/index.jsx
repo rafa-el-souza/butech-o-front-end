37c29580d3a2240283e49ec542060444ede54074
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-24 18:52:49 -0300

    feat: add global context providers and main app route

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import App from './App';
+import './index.css';
+
+import { ProductsProvider } from './contexts/ProductsContext';
+import { CategoriesProvider } from './contexts/CategoriesContext';
+import { ShoppingCartProvider } from './contexts/ShoppingCartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      {/* Global contexts goes inside here */}
+      <ShoppingCartProvider>
+        <ProductsProvider>
+          <CategoriesProvider>
+            <Routes>
+              <Route path="/*" element={<App />} />
+            </Routes>
+          </CategoriesProvider>
+        </ProductsProvider>
+      </ShoppingCartProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
