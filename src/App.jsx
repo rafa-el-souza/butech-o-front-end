ba60a9424abef8f1c34c82243706d38b0aaff27c
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-24 18:54:58 -0300

    feat: add routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-const App = () => {
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import ProductDetails from './pages/ProductDetails';
+import ShoppingCart from './pages/ShoppingCart';
+import Checkout from './pages/Checkout';
+import Delivery from './pages/Delivery';
+import Menu from './pages/Menu';
+
+import Layout from './components/Layout';
+
+function App() {
   return (
-    <div className="App">
-      <h1>butech-o</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Menu />} />
+        <Route path="details/:id" element={<ProductDetails />} />
+        <Route path="cart" element={<ShoppingCart />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="delivery" element={<Delivery />} />
+        <Route path="*" element={<Menu />} />
+      </Route>
+      <Route path="*" element={<Layout />}>
+        <Route path="*" element={<Menu />} />
+      </Route>
+    </Routes>
   );
 }
 
