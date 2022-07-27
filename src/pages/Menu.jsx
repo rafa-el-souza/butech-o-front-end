4988274d3daac587c31431b4949d32079c41fb37
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-27 18:50:12 -0300

    feat: add components to menu page

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,54 +1,43 @@
 import React from 'react';
 
-// import MenuBackgroundPhoto from '../components/MenuBackgroundPhoto';
-// import MenuHeader from '../components/MenuHeader';
-// import MenuLogo from '../components/MenuLogo';
-// import MenuOpenClosed from '../components/MenuOpenClosed';
-// import MenuSearchBar from '../components/MenuSearchBar';
-// import MenuTitle from '../components/MenuTitle';
-// import SearchByCategory from '../components/SearchByCategory';
-// import SearchByKeyword from '../components/SearchByKeyword';
+import MenuHeader from '../components/MenuHeader';
+import MenuLogo from '../components/MenuLogo';
+import MenuOpenClosed from '../components/MenuOpenClosed';
+import MenuTitle from '../components/MenuTitle';
+import MenuBackgroundPhoto from '../components/MenuBackgroundPhoto';
+import MenuSearchBar from '../components/MenuSearchBar';
+import SearchByCategory from '../components/SearchByCategory';
+import SearchByKeyword from '../components/SearchByKeyword';
+import MenuCategoryList from '../components/MenuCategoryList';
 
 function Menu() {
   return (
-    <div>
-      Menu
-      {/* <MenuHeader>
+    <main>
+      <MenuHeader>
         <MenuLogo
+          className="menu-logo"
           logoURL="https://upload.wikimedia.org/wikipedia/commons/a/ab/Logo_TV_2015.png"
-          className="w-16 bg-slate-100 absolute top-48 left-8"
         />
         <MenuOpenClosed
-          className="absolute text-gray-200 bg-green-600 right-0 top-36"
+          className="menu-open-closed"
         />
         <MenuTitle
+          className="menu-title"
           title="butech-o"
-          className="absolute text-gray-200 text-2xl left-32 top-56"
         />
         <MenuBackgroundPhoto
+          className="menu-background-photo"
           bgPhotoURL="https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg"
-          className="relative w-screen -z-10"
         />
-        <MenuSearchBar>
-          <SearchByKeyword />
-          <SearchByCategory />
-        </MenuSearchBar>
       </MenuHeader>
-      <MenuCategoryList>
-        <MenuCategory>
-          <MenuProductList>
-            <MenuProduct>
-              <MenuProductPhoto />
-              <MenuProductTitle />
-              <MenuProductDescription />
-              <MenuProductPrice />
-              <CartInput />
-            </MenuProduct>
-          </MenuProductList>
-        </MenuCategory>
-      </MenuCategoryList> */}
 
-    </div>
+      <MenuSearchBar>
+        <SearchByKeyword />
+        <SearchByCategory />
+      </MenuSearchBar>
+
+      <MenuCategoryList />
+    </main>
   );
 }
 
