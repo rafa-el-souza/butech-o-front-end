014cdd2f2419aa6598884b3abbf6a5cee6985b19
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-27 18:40:04 -0300

    feat: add style to search by keyword component

--- src/components/SearchByKeyword.jsx
+++ src/components/SearchByKeyword.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 function SearchByKeyword() {
   return (
-    <form>
-      <input type="text" name="" id="" />
-    </form>
+    <span className="search-by-keyword-container">
+      <form className="search-by-keyword-form">
+        <input
+          className="search-by-keyword-input"
+          type="text"
+          name=""
+          id=""
+        />
+      </form>
+    </span>
   );
 }
 
