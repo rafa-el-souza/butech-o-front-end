1cb084a28c60a1434dbfecf30580e86687d91e36
rafa-el-souza <rafaelmvsouza@gmail.com>
2022-07-30 07:10:50 -0300

    refactor: temporarily remove navbar from layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import NavBar from './NavBar';
+// import NavBar from './NavBar';
 
 function Layout() {
   return (
-    <main className="App">
-      <NavBar />
+    <div className="App">
+      {/* <NavBar /> */}
       {/* Any components apart from <Outlet/> will be rendered in all child routes. */}
       <Outlet />
       {/* <Outlet/> acts as a placeholder for child routes.
       Active child routes are rendered here. */}
-    </main>
+    </div>
   );
 }
 
